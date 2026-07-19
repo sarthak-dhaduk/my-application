8ec608470a065266af2716dfa8ccafbb644ab392
Sarthak Dhaduk <sarthakdhaduk1111@gmail.com>
2026-07-19 11:40:16 +0530

    demo #1

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-bold text-red-800">Hello</Text>
+    </View>
+  );
+}
